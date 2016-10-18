58c187b71fffd4dae02e2b2f4a847a461d43968d
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2016-10-17 17:00:28 -0700

    Apply another test change for debugging

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var port = 3000;
 User.sync({ force: true })
   .then(function () {
     console.log('Users table created');
-    return User.create({ username: 'dylan' });
+    return User.create({ username: 'me' });
   })
   .then(function() {
     console.log('Seeded User table');
