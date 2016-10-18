a8fd485e05844ac70116233acde1f2d863ad3228
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2016-10-17 17:28:30 -0700

    Require bcrypt and bodyParser dependancies

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var port = 3000;
 User.sync({ force: true })
   .then(function () {
     console.log('Users table created');
-    return User.create({ username: 'me' });
+    return User.create({ username: 'me', password: 'pass' });
   })
   .then(function() {
     console.log('Seeded User table');
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var bcrypt = require('bcrypt-nodejs');
+var bodyParser = require('body-parser');
 var app = express();
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize('teleporterDB', 'root', '123', { 
@@ -6,7 +8,8 @@ var sequelize = new Sequelize('teleporterDB', 'root', '123', {
 });
 
 var User = sequelize.define('User', {
-  username: Sequelize.STRING
+  username: Sequelize.STRING,
+  password: Sequelize.STRING
 });
 
 /*  Create a '/users' route that responds to 
@@ -15,6 +18,12 @@ var User = sequelize.define('User', {
 app.get('/users', function(req, res) {
   User.findAll().then(function(users) {
     res.send(users);
+  });
+});
+
+app.set('/users', function(req, res) {
+  User.create().then(function(users) {
+    res.send('user created');
   });
 });
 
